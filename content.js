ee4d9321479f20dbc77f8a89d88766dc3f1a7443
Kunal Goel <goelkun@hotmail.com>
2019-11-11 17:48:01 +0530

    Fixed small bug with utf-8 decoder

--- content.js
+++ content.js
@@ -67,10 +67,15 @@ function is_url(str) {
 }
 
 function b64DecodeUnicode(str) {
-    // Going backwards: from bytestream, to percent-encoding, to original string.
-    return decodeURIComponent(atob(str).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+    try {
+        // Going backwards: from bytestream, to percent-encoding, to original string.
+        return decodeURIComponent(atob(str).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    } catch (e) {
+        console.log(e);
+		return str;
+    }
 }
 
 chrome.runtime.onMessage.addListener(
